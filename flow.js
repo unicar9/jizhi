2b765e5d3b49557c5c44242819c151d96ec43406
Lingxiao Wang <xwangl213@gmail.com>
2019-03-20 18:51:02 +1100

    added

--- flow.js
+++ flow.js
@@ -1,48 +1,80 @@
 console.log("flow.js")
 
-let scl = 20, 
-    inc = 0.01,
-    cols,
-    rows
+let scl = 20,
+	inc = 0.01,
+	cols,
+	rows,
+	particles = [],
+	flowfield = []
+
+class Particle {
+	constructor() {
+		this.pos = createVector(0, 0)
+		this.vel = createVector(0, 0)
+		this.acc = createVector(0, 0)
+	}
+
+	update() {
+		this.pos.add(this.vel)
+		this.vel.add(this.acc)
+		this.acc.mult(0)
+	}
+
+	applyForce(force) {
+
+		this.acc.add(force)
+
+	}
+
+	display() {
+
+	}
+
+	follow() {
+		
+	}
+
+}
+
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+	createCanvas(windowWidth, windowHeight)
 
-  cols = floor( width / scl )
-  rows = floor( height / scl )
+	cols = floor(width / scl)
+	rows = floor(height / scl)
 
 }
 
 let t = 0
 
 function draw() {
-  background(220)
+	background(220)
 
 
-  let yoff = 0
-  for (let x = 0; x < cols; x++) {
-    let xoff = 0
-    for (let y = 0; y < rows; y++) {
+	let yoff = 0
+	for (let x = 0; x < cols; x++) {
+		let xoff = 0
+		for (let y = 0; y < rows; y++) {
 
-      noiseDetail( 2, 1.8)
-      let n = noise(xoff, yoff) 
-      let angle = n * TWO_PI
-      let v = p5.Vector.fromAngle( angle )
-      xoff += inc
+			noiseDetail(2, 1.8)
+			let n = noise(xoff, yoff)
+			let angle = n * TWO_PI
+			let v = p5.Vector.fromAngle(angle)
+			xoff += inc
 
-      stroke( n * 56, n * 128, n * 25, n * 255)
+			stroke(n * 56, n * 128, n * 25, n * 255)
 
-      push()
-        translate( x * scl, y * scl )
-        rotate( v.heading() )
-        // ellipse(0, 0, scl)
-        line(0, 0, scl, 0)
-      pop()
+			push()
+			translate(x * scl, y * scl)
+			rotate(v.heading())
+			line(0, 0, scl, 0)
+			pop()
 
-    }
-    yoff += inc    
-  }
+		}
+		yoff += inc
 
+	}
 
 
 }

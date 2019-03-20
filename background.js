2484730c2e47e901709ffe1a0267c3c445d60fff
Unicar <unicar9@gmail.com>
2019-03-20 20:02:07 +1100

    Updated

--- background.js
+++ background.js
@@ -4,8 +4,6 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-});
+chrome.browserAction.onClicked.addListener(function(){
+  chrome.tabs.create({"url": "chrome://newtab"})
+})

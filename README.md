6cccb703ea488b4bbe2b8639f1ce42fccb6b7b11
Unicar <unicar9@gmail.com>
2019-03-22 15:42:42 +1100

    Updated manifest.json file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# jizhi
+# 几枝 | Jizhi
+
+支持自定义新标签页的 Chrome 扩展，几枝将在新标签页上展示中国传统色的层叠波浪动画效果搭配经典诗词。
+
 A Chrome extension with custom new tab page featuring Chinese poems and P5.js enabled noise waves in Chinese traditional colors
+
+## Examples
+
--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+'use strict'
 
 chrome.browserAction.onClicked.addListener(function(){
   chrome.tabs.create({"url": "chrome://newtab"})

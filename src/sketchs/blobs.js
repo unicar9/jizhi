d681ba80de946f4e3d350befb19020d2e6a60687
Unicar <unicar9@gmail.com>
2019-05-14 22:21:17 +1000

    Adjusting blobs params

--- src/sketchs/blobs.js
+++ src/sketchs/blobs.js
@@ -8,7 +8,7 @@ export default function blobs (p) {
 
   p.draw = function () {
     p.clear()
-    p.background(220)
+    p.background(230)
     p.noStroke()
 
     blobsArray.forEach(blob => blob.display(p))
@@ -37,19 +37,21 @@ class Blob {
 
   display (p) {
     p.push()
-    p.fill(this.c)
+    let color = p.color(this.c)
+    color.setAlpha(230)
+    p.fill(color)
     p.translate(this.x, this.y)
 
     this.s = p.lerp(this.s, 1, 0.07)
     p.scale(this.s)
 
-    p.noiseDetail(1, 0.8)
+    p.noiseDetail(2, 0.9)
     p.beginShape()
     for (let i = 0; i < p.TWO_PI; i += p.radians(1)) {
       let xOff = this.offset * p.cos(i) + this.offset
       let yOff = this.offset * p.sin(i) + this.offset
 
-      let r = 200 + p.map(p.noise(xOff, yOff, this.t), 0, 1, -this.scale, this.scale)
+      let r = 230 + p.map(p.noise(xOff, yOff, this.t), 0, 1, -this.scale, this.scale)
       let x = r * p.cos(i)
       let y = r * p.sin(i)
 
@@ -64,19 +66,18 @@ class Blob {
 function generateBlobs (p) {
   const colors = ['#1383d8', '#53ace2', '#6e6cd1', '#fcd09f']
 
-  const offset = p.random(0.2, 0.8)
+  const offset = p.random(0.2, 0.9)
 
   new Array(4).fill(1).map((_, i) => {
-    const scale = p.random(20, 80)
+    const scale = p.random(20, 60)
 
-    const x = (p.windowWidth / 2 * (i % 2)) + p.random(0, 500)
-    const y = (p.windowHeight / 2 * (i % 2)) + p.random(0, 500)
+    const x = (i % 2) ? (p.width / 4 + p.random(-300, 0)) : (p.width / 4 * 3 + p.random(0, 300))
+    const y = (i < 2) ? (p.height / 4 + p.random(-100, 0)) : (p.height / 4 * 3 + p.random(0, 100))
 
-    const tSpeed = p.random(0.02, 0.1)
+    const tSpeed = p.random(0.02, 0.06)
     const color = colors[i % 4]
 
     let blob = new Blob(offset, scale, x, y, tSpeed, color)
-    console.log('blob', blobsArray)
     blobsArray.push(blob)
   })
 }

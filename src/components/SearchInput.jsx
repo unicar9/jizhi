d87946abf277a2020f46d68452c214961bda8886
Arthur Zheng <ArthurZheng@porch.com>
2019-07-22 16:35:51 -0700

    made SearchInput  a pure component

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,33 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Icon } from 'evergreen-ui'
-import PropTypes from 'prop-types'
+import { string, bool, func } from 'prop-types'
 
-class SearchInput extends Component {
-  render () {
-    const { engineOption, value, focused, handleFocus, handleBlur, handleChange } = this.props
-    return (
-      <form id='jizhi-search' action={engineOption.split('?')[0]} data-html2canvas-ignore>
-        <Icon id='jizhi-search-icon' icon='search' size={16} />
-        <input
-          className={(focused || value) ? 'active' : null}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          name={engineOption.split('.')[1] === 'baidu' ? 'wd' : 'q'}
-          autocomplete='off'
-        />
-      </form>
-    )
-  }
-}
+const SearchInput = ({ engineOption, value, focused, handleFocus, handleBlur, handleChange }) => (
+  <form id='jizhi-search' action={engineOption.split('?')[0]} data-html2canvas-ignore>
+    <Icon id='jizhi-search-icon' icon='search' size={16} />
+    <input
+      className={(focused || value) ? 'active' : null}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      name={engineOption.split('.')[1] === 'baidu' ? 'wd' : 'q'}
+      autocomplete='off'
+    />
+  </form>
+)
 
 SearchInput.propTypes = {
-  value: PropTypes.string,
-  focused: PropTypes.bool,
-  engineOption: PropTypes.string,
-  handleFocus: PropTypes.func,
-  handleBlur: PropTypes.func,
-  handleChange: PropTypes.func
+  value: string,
+  focused: bool,
+  engineOption: string,
+  handleFocus: func,
+  handleBlur: func,
+  handleChange: func
 }
 
 export default SearchInput

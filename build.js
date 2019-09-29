4e32d76bb18652c43100ea60e20ffcc30df820e9
Lingxiao Wang <xwangl213@gmail.com>
2019-09-29 11:47:22 +1000

    Updated build.js to enable auto zip files for furthur release

--- build.js
+++ build.js
@@ -1,54 +1,67 @@
 const chalk = require('chalk')
-
-console.log(chalk.bgMagenta(' 我是大傻子'))
-
 const fs = require('fs')
+const path = require('path')
 const archiver = require('archiver')
-const chalk = require('chalk')
+const log = console.log
 
-const output = fs.createWriteStream(__dirname + '/chrome.zip')
-
-const archive = archiver('zip', {
-    zlib: { level: 9 } 
-})
-
-output.on('close', () => {
-    console.log(chalk.green((archive.pointer() / 1048576).toFixed(2) + ' MB'))
-    console.log('archiver has been finalized and the output file descriptor has closed.')
-})
-
-
-output.on('end', () => {
-    console.log('Data has been drained')
-})
-
-
-archive.on('warning', function(err) {
-    if (err.code === 'ENOENT') {
-        console.log('EEEEEE')
-    } else {
-        throw err
+const browsers = {
+    chrome: {
+        name: 'chrome',
+        labelColor: 'magenta'
+    },
+    firefox: {
+        name: 'firefox',
+        labelColor: 'yellow'
     }
-})
-
-
-archive.on('error', function(err) {
-    throw err
-})
-
-archive.pipe(output)
-
-const buildDir = __dirname + '/build/'
-
-
-if (buildDir) {
-    console.log("Building: " + buildDir)
-    archive.directory('build/', false)
-} else {
-    chalk.yellow('Build folder not found')
 }
 
 
-archive.finalize()
+const getArchive = browser => {
+    const buildFolderPath = path.resolve(__dirname, `./builds/build_${browser}`)
 
+    if (!fs.existsSync(buildFolderPath)) {
+        log(chalk.yellow(`${bowser} build folder NOT found...`))
+        return
+    }
+
+    const chalkLabel = chalk.keyword(`${browsers[browser].labelColor}`)
+    const label = chalkLabel(browser)
+
+    log(label, chalk.green(`${browser} build folder found...`))
+
+    const zipPath = path.resolve(__dirname, `./builds/${browser}.zip`)
+    const output = fs.createWriteStream(zipPath)
+    
+    const archive = archiver('zip', {
+        zlib: { level: 9 } 
+    })
+
+    output.on('close', () => {
+        const fileSizeMB = (archive.pointer() / 1048576).toFixed(2)
+        log(label, chalk.green(`Zip file size: ${fileSizeMB} MB`))
+        log(label, 'archiver has been finalized and the output file descriptor has closed.')
+    })
+
+    output.on('end', () => log(label, 'Data has been drained'))
+
+
+    archive.on('warning', err => {
+        if (err.code === 'ENOENT') {
+            log(label, 'EEEEEE')
+        } else {
+            throw err
+        }
+    })
+    
+    archive.on('error', err => {
+        throw err
+    })
+    
+    archive.pipe(output)
+    archive.directory(buildFolderPath, false)
+    archive.finalize()
+}
+
+getArchive('chrome')
+getArchive('firefox')
 

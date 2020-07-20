a38643e93d034dde5973abe47d8c7efe17c450b2
Tim <wangtian0312@gmail.com>
2020-07-19 20:56:45 -0400

    chore: add legal info

--- src/components/ConfigMenu/Legal.jsx
+++ src/components/ConfigMenu/Legal.jsx
@@ -5,10 +5,9 @@ const Legal = () => (
   <div style={{ height: 50 }}>
     <Text margin={20} lineHeight='50px' color='muted'>
       本扩展使用了
-      <a href='https://www.jinrishici.com/' target='_blank' rel='noopener noreferrer'>
-        今日诗词 API
-      </a>
-      ，感谢您的支持和喜爱!
+      <a href='https://www.jinrishici.com/' target='_blank' rel='noopener noreferrer'>今日诗词 API </a>
+      与
+      <a href='https://github.com/pwxcoo/chinese-xinhua' target='_blank' rel='noopener noreferrer'> Github 新华字典数据库</a>
     </Text>
   </div>
 )

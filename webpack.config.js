aca5d6e533f4eac4d988028220a8e56ae84439b7
Unicar <unicar9@gmail.com>
2021-02-25 18:04:00 +1100

    small tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 // Whilst the configuration object can be modified here, the recommended way of making
 // changes is via the presets' options or Neutrino's API in `.neutrinorc.js` instead.
 // Neutrino's inspect feature can be used to view/export the generated configuration.
-const neutrino = require('neutrino');
 const path = require('path');
+const neutrino = require('neutrino');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const basicConfig = neutrino().webpack();
